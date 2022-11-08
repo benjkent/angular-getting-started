1a969490d1d37c94661cf3dccac90ce3e0476c69
benjkent <68797909+benjkent@users.noreply.github.com>
2022-11-08 13:11:21 -0800

    create a cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     //First get the product id from the current route.
@@ -22,4 +27,10 @@ export class ProductDetailsComponent implements OnInit {
     );
   }
   product: Product | undefined;
+
+  // Define the cartService's methods
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert(product.name + ' has beed added to the cart!');
+  }
 }
